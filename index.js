41c6b21eba54a9b08123ec162098b8ed3f595580
Bradley <bradsamsess90@icloud.com>
2022-12-09 21:58:35 -0500

    floders/flies/jest/test.js(s) all added

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateHtml = require('./src/generatehtml');
 
+function init(){
+  inquirer.prompt(
+      { type: 'list',
+      message: 'Please create your team?',
+      name: 'startQuestion',
+      choices: ["Manager", "Team members","Intern ","Quit"]
+      }) .then((response) =>{
+      let choices = response.startQuestion
+       // use a switch statement to pick the appropiate fuction for the user selction
+      switch(choices){
+          case "Manger":
+              manager();
+              break;
+
+          case "Team members":
+              teamMembers();
+              break;
+
+          case "Intern":
+              Intern();
+              break;
+          case "Quit":
+              quit();
+              break;
+      }
+  });
+}
+init();
+
+
+ module.exports = generateHtml
