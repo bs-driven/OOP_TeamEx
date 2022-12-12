7430d7cafa126ed91064d199b939df04b4e1d70c
Bradley <bradsamsess90@icloud.com>
2022-12-11 22:10:57 -0500

    employee and engineer test working with toEqual, manager and intern test next

--- Main/test/Engineer.test.js
+++ Main/test/Engineer.test.js
@@ -3,11 +3,21 @@ const Engineer = require('../lib/Engineer');
 describe('Engineer',()=>{
     
     const employee = new Engineer('Monkey D. Luffy', 787848, 'kingofpirates@gmail.com', 'githubAccount')
-        test("test name , id , github", ()=> {
-            expect(employee.name).stringMatching('Monkey D. Luffy')
-            expect(employee.id).toEqual(787848)
-            expect(employee.email).stringMatching('kingofpirates@gmail.com')
-            expect(employee.gitHub).stringMatching('githubAccount')
-        })
+
+       it("should return the engineers name", () => {
+        expect(employee.name).toEqual('Monkey D. Luffy');
+       });
+
+       it("should return the engineers id number", () =>{
+        expect(employee.id).toEqual(787848)
+       });
+
+       it('should return the engineers email address', () => {
+        expect(employee.email).toEqual('kingofpirates@gmail.com')
+       });
+       it('should return the github account of the engineer', () => {
+        expect(employee.gitHub).toEqual('githubAccount')
+       })
+
     
 })
