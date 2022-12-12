769d7f3b6fe7067fc290e58437cbf9523bf71ca5
Bradley <bradsamsess90@icloud.com>
2022-12-12 07:20:53 -0500

    intern test complete with properties and methods/functions

---
+++ Main/test/Intern.test.js
@@ -0,0 +1,47 @@
+const Intern = require("../lib/Intern")
+
+describe("Intern properties", ()=>{
+    const employee = new Intern('Killua Zoldic', 546952, 'silentkill@gmail.com', 'OSU')
+    it("should return the intern's name", () =>{
+        expect(employee.name).toEqual('Killua Zoldic')
+    });
+
+    it("should return the intern's id number", () =>{
+        expect(employee.id).toEqual(546952)
+    });
+
+    it("should return the intern's email address",()=>{
+        expect(employee.email).toEqual('silentkill@gmail.com')
+    });
+
+    it("should returns the intern's school", () =>{
+        expect(employee.school).toEqual('OSU')
+    });
+})
+
+
+describe("Intern methods",() =>{
+    const employee = new Intern('Killua Zoldic', 546952, 'silentkill@gmail.com', 'OSU')
+
+    it("should return the interns name", () =>{
+        expect(employee.getName()).toEqual('Killua Zoldic')
+    });
+
+    it("hould returns the intern's id number", () =>{
+        expect(employee.getId()).toEqual(546952)
+    });
+
+    it("should return the interns email address", () => {
+        expect(employee.getEmail()).toEqual('silentkill@gmail.com')
+    });
+
+    it("should return the school that the intern attends", () =>{
+        expect(employee.getSchool()).toEqual('OSU')
+    });
+
+    it("should return the interns role", () =>{
+        expect(employee.getRole()).toEqual("Intern")
+    });
+
+
+})
