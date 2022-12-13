0bb7021b1b4ff9d3e4e963291b156f6b84fd6656
Bradley <bradsamsess90@icloud.com>
2022-12-12 20:33:27 -0500

    index and generate html wrote. questions not working properly tho

---
+++ Main/src/generatehtml.js
@@ -0,0 +1,111 @@
+const managerCard = function (manager) {
+    return `
+    <div class="card m-3" style="width: 18rem;">
+      <div class="card-header text-white bg-primary mb-3">
+        <h3>${manager.name}</h3>
+        <h4>Manager</h4>
+      </div>
+      <div class="card-body">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">ID: ${manager.id}</li>
+          <li class="list-group-item">Email: <a href = "mailto:${manager.email}">${manager.email}</a></li>
+          <li class="list-group-item">Office number: ${manager.officeNumber}</li>
+        </ul>
+      </div>
+    </div>
+    `
+};
+
+const engineerCard = function (engineer) {
+    return `
+    <div class="card m-3" style="width: 18rem;">
+      <div class="card-header text-white bg-primary mb-3">
+        <h3>${engineer.name}</h3>
+        <h4>Engineer</h4>
+      </div>
+      <div class="card-body">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">ID: ${engineer.id}</li>
+          <li class="list-group-item">Email: <a href = "mailto:${engineer.email}">${engineer.email}</a></li>
+          <li class="list-group-item">GitHub: <a href = "https://github.com/${engineer.gitHub}" target="_blank">${engineer.gitHub}</a></li>
+        </ul>
+      </div>
+    </div>
+    `
+  };
+
+  const internCard = function (intern) {
+    return `
+    <div class="card m-3" style="width: 18rem;">
+      <div class="card-header text-white bg-primary mb-3">
+        <h3>${intern.name}</h3>
+        <h4>Intern</h4>
+      </div>
+      <div class="card-body">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">ID: ${intern.id}</li>
+          <li class="list-group-item">Email: <a href = "mailto:${intern.email}">${intern.email}</a></li>
+          <li class="list-group-item">School: ${intern.school}</li>
+        </ul>
+      </div>
+    </div>
+    `
+  };
+
+  const htmlPage = function (teamCards) {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
+      <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+      <link rel="stylesheet" href="./style.css">
+      <title>My Team</title>
+    </head>
+    <body>
+      <header>
+      <div class="jumbotron text-center">
+        <h1 class="display-4">My Team</h1>
+      </div>
+      </header>
+      <div class="container">
+        <div class="row justify-content-center" id="cards">
+          ${teamCards}
+        </div>
+      </div>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+      <script src="https://code.jquery.com/ui/1.12.0/jquery-ui.min.js"></script>
+    </body>
+    </html>
+    `
+  };
+
+  generateHtml = (team) => {
+  
+    const teamArray = [];
+    
+    for (let i = 0; i< team.length; i++) {
+      const member = team[i];
+      const role = member.getRole();
+      if (role === 'Manager') {
+        const manager = managerCard(member);
+        teamArray.push(manager);
+      } else if (role === 'Engineer') {
+        const engineer = engineerCard(member);
+        teamArray.push(engineer);
+      } else if (role === 'Intern') {
+        const intern = internCard(member);
+        teamArray.push(intern);
+      }
+    }
+  
+    const teamCards = teamArray.join('');
+    const html = htmlPage(teamCards);
+    // console.log(teamCards)
+    return html;
+  }
--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const Employee = require('./Main/lib/Employee');
 const Engineer = require('./Main/lib/Engineer');
 const Intern = require('./Main/lib/Intern');
 const Manager = require('./Main/lib/Manager');
-const generateHtml = require('./src/generatehtml');
+const generateHtml = require('./Main/src/generatehtml');
+const team = []
 
-function starterQuestions(){
+function startQuestion () {
   inquirer.prompt(
       { type: 'list',
-      message: 'Please create your team?',
+      message: 'Please select a team member to introduce?',
       name: 'startQuestion',
       choices: ["Manager", "Engineer","Intern ","Quit"]
       }) .then((response) =>{
@@ -17,22 +18,135 @@ function starterQuestions(){
        // use a switch statement to pick the appropiate fuction for the user selction
       switch(choices){
           case "Manger":
-              manager();
+              managerInfo();
               break;
 
-          case "Team members":
-              enginner();
+          case "Engineer":
+            engineerInfo();
               break;
 
           case "Intern":
-              intern();
+              internInfo();
               break;
           case "Quit":
               quit();
               break;
       }
   });
+};
+
+
+
+function managerInfo() {
+    inquirer.propmt(
+        {
+            type: 'input',
+            message: 'Please enter the managers name.',
+            name: 'managerName'
+        },
+        {
+            type: 'input',
+            message: 'Please enter the managers id number.',
+            name:'managerID'
+        },
+        {
+            type: 'input',
+            message: 'Please enter the managers email address.',
+            name: 'managerEmail'
+        },
+        {
+            type: 'input',
+            message: 'Please enter the mangers office number',
+            name: 'managerOfficeNumber'
+        }
+    ).then(response =>{
+        const manager = new Manager(response.managerName, response.managerID, response.managerEmail, response.mangerOfficeNumber);
+        team.push(manager);
+        fs.writeFile('',generateHtml(manager),(err)=>{
+            if(err)
+            console.log(err);
+            else{
+                console.log("File written succefully")
+            }
+        })
+    })
 }
-init();
+
+function engineerInfo() {
+    inquirer.prompt(
+        {
+            type: 'input',
+            message: 'Please enter the engineers name.',
+            name: 'engineerName'
+        },
+        {
+            type: 'input',
+            message: 'Please enter the engineeers id number.',
+            name:'engineerID'
+        },
+        {
+            type: 'input',
+            message: 'Please enter the engineers email address.',
+            name: 'engineerEmail'
+        },
+        {
+            type: 'input',
+            message: 'please enter th4e engineers github account name.',
+            name: 'engineerGithubName'
+        }        
+    ).then(response =>{
+        const engineeer = new Engineer(response.engineeerName, response.engineeerID, response.engineeerEmail, response.engineeerGithubName);
+        team.push(engineeer);
+        fs.writeFile('',generateHtml(engineeer),(err)=>{
+            if(err)
+            console.log(err);
+            else{
+                console.log("File written succefully")
+            }
+        })
+    })
+
+};
+
+function internInfo() {
+    inquirer.prompt(
+        {
+            type: 'input',
+            message: 'Please enter the interns name.',
+            name: 'internName'
+        },
+        {
+            type: 'input',
+            message: 'Please enter the interns id number.',
+            name:'internID'
+        },
+        {
+            type: 'input',
+            message: 'Please enter the interns email address.',
+            name: 'internEmail'
+        },
+        {
+            type: 'input',
+            message: 'Please enter the intern school.',
+            name: 'internSchool'
+        }
+    ).then(response => {
+        const intern = new Intern(response.internName, response.internID, response.internEmail, response.internSchool);
+        team.push(intern);
+        fs.writeFile('',generateHtml(intern),(err)=>{
+            if(err)
+            console.log(err);
+            else{
+                console.log("File written succefully")
+            }
+        })
+
+    })
+
+}
+
+console.log(team)
+
+startQuestion();
 
 
